32628f8767c818147c7dfa24623af394c41bdc92
raghueswar55 <raghueswar55@gmail.com>
2020-09-08 09:20:17 +0530

    [ Raghu ] REFACTORED: implemented router for login page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import "./App.css";
+import SignIn from "./components/SignIn";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact>
+        <Redirect to="login" />
+      </Route>
+      <Route path="/login" exact>
+        <SignIn></SignIn>
+      </Route>
+    </Router>
   );
 }
 
