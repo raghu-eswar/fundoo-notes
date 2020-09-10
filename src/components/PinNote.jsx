a377261c47fe83a0a54a11d9e68d6b0d385b97fc
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 17:48:28 +0530

    [ Raghu ] ADD: implemented component to pin a note.

---
+++ src/components/PinNote.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import PinDropIcon from "@material-ui/icons/PinDrop";
+
+export default function PinNote(props) {
+  return (
+    <IconButton>
+      <PinDropIcon />
+    </IconButton>
+  );
+}
