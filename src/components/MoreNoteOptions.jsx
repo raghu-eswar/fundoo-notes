3bc2a3172b0104f37740a185088b128626744eca
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 18:01:38 +0530

    [ Raghu ] ADD: implemented component to show more options for a note.

---
+++ src/components/MoreNoteOptions.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+export default function MoreNoteOptions(props) {
+  return (
+    <IconButton>
+      <MoreVertIcon />
+    </IconButton>
+  );
+}
