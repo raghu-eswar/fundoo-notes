7f901ad57a54b0a6ca48f14f480f9f690a319f08
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 21:08:22 +0530

    [ Raghu ] REFACTORED: implemented router for home page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import "./App.css";
 import SignIn from "./components/SignIn";
+import Dashboard from './pages/Dashboard.jsx';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       </Route>
       <Route path="/login" exact>
         <SignIn></SignIn>
+      </Route>
+      <Route path="/home" exact>
+        <Dashboard></Dashboard>
       </Route>
     </Router>
   );
