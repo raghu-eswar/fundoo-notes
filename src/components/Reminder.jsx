2205d80f57383556e2d5e23ff204a670778c22e0
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 17:54:06 +0530

    [ Raghu ] ADD: implemented component to add reminder to a note.

---
+++ src/components/Reminder.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import AddAlertSharpIcon from "@material-ui/icons/AddAlertSharp";
+
+export default function Reminder(props) {
+  return (
+    <IconButton>
+      <AddAlertSharpIcon />
+    </IconButton>
+  );
+}
