668621a0c77d48d326a05c59b384630b57f921a3
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 17:57:34 +0530

    [ Raghu ] ADD: implemented component to collaborate a note.

---
+++ src/components/Collaborate.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import GroupAddOutlinedIcon from '@material-ui/icons/GroupAddOutlined';
+
+export default function Collaborate(props) {
+  return (
+    <IconButton>
+      <GroupAddOutlinedIcon />
+    </IconButton>
+  );
+}
