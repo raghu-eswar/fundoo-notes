245c20b99e78fcea54d81b7290c44ad1850029b4
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 17:49:37 +0530

    [ Raghu ] ADD: implemented component to add color to a note.

---
+++ src/components/AddColor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import ColorLensIcon from '@material-ui/icons/ColorLens';
+
+export default function AddColor(props) {
+  return (
+    <IconButton>
+      <ColorLensIcon />
+    </IconButton>
+  );
+}
