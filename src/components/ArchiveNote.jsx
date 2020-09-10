abf70734d7234ded098bf54b96fa4080dada4488
raghueswar55 <raghueswar55@gmail.com>
2020-09-10 17:56:13 +0530

    [ Raghu ] ADD: implemented component to move a note to archive.

---
+++ src/components/ArchiveNote.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import IconButton from "@material-ui/core/IconButton";
+import ArchiveIcon from '@material-ui/icons/Archive';
+
+export default function ArchiveNote(props) {
+  return (
+    <IconButton>
+      <ArchiveIcon />
+    </IconButton>
+  );
+}
