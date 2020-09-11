3f5c177faf9c4b1f31b2c7a4ecd92c762204800c
raghueswar55 <raghueswar55@gmail.com>
2020-09-11 17:57:52 +0530

    [ Raghu ] REFACTORED: implemented api calls to add new note.

--- src/services/notesServices.js
+++ src/services/notesServices.js
@@ -1,6 +1,12 @@
 import axios from 'axios';
 import { notesApiConstants } from '../apiConstants/notesApiConstants';
 
+export function addNotes(note, token) {
+    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.addNotes, note, {
+        headers: {Authorization: token,}
+    })
+}
+
 export function getNotesList(token) {
     return axios.get(process.env.REACT_APP_API_BASE_URL + notesApiConstants.getNotesList, {
         headers: {Authorization: token,}
