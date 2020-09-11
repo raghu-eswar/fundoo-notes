17c648d288d254699a007ade0484079265e8b02a
raghueswar55 <raghueswar55@gmail.com>
2020-09-11 16:15:46 +0530

    [ Raghu ] ADD: implemented note component to display notes.

---
+++ src/components/Note.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import PinNote from "./PinNote";
+import Reminder from "./Reminder";
+import Collaborate from "./Collaborate";
+import AddColor from "./AddColor";
+import AddImage from "./AddImage";
+import ArchiveNote from "./ArchiveNote";
+import MoreNoteOptions from "./MoreNoteOptions";
+import * as Styled from "../styles/note.styled";
+
+export default function Note(props) {
+  return (
+    <Styled.NoteContainer className="note">
+      <CardContent>
+        <Styled.TitleContainer>
+          <Typography variant="h6" align="left">
+            {props.note.title}
+          </Typography>
+          <PinNote />
+        </Styled.TitleContainer>
+        <Typography variant="subtitle1" gutterBottom align="left">
+          {props.note.description.split(" ").splice(0, 30).join(" ")}
+        </Typography>
+        <Styled.DescriptionContainer>
+          <Reminder />
+          <Collaborate />
+          <AddColor />
+          <AddImage />
+          <ArchiveNote />
+          <MoreNoteOptions />
+        </Styled.DescriptionContainer>
+      </CardContent>
+    </Styled.NoteContainer>
+  );
+}
---
+++ src/styles/note.styled.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import Card from "@material-ui/core/Card";
+
+export const NoteContainer = styled(Card)`
+  width: 15rem;
+  cursor: pointer;
+  button {
+    padding: 6px;
+    visibility: hidden;
+  }
+  button .MuiSvgIcon-root {
+    font-size: 1.3rem;
+  }
+  &:hover button {
+    visibility: visible;
+  }
+`;
+export const TitleContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 10px 0;
+`;
+
+export const DescriptionContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+`;
