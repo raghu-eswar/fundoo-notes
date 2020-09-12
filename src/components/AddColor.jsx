797cc599ba81c857fdfabc3eb7c682714cb54bf7
raghueswar55 <raghueswar55@gmail.com>
2020-09-12 09:09:40 +0530

    [ Raghu ] REFACTORED: modified to display color picker on click.

--- src/components/AddColor.jsx
+++ src/components/AddColor.jsx
@@ -1,11 +1,61 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import ColorLensIcon from '@material-ui/icons/ColorLens';
+import ColorLensIcon from "@material-ui/icons/ColorLens";
+import Popover from "@material-ui/core/Popover";
+import { CirclePicker } from "react-color";
+
+const colors = [
+  "#ffffff",
+  "#f44336",
+  "#e91e63",
+  "#9c27b0",
+  "#673ab7",
+  "#3f51b5",
+  "#2196f3",
+  "#03a9f4",
+  "#00bcd4",
+  "#009688",
+  "#4caf50",
+  "#8bc34a",
+  "#cddc39",
+  "#ffeb3b",
+  "#ffc107",
+  "#ff9800",
+  "#ff5722",
+  "#795548",
+  "#607d8b",
+];
 
 export default function AddColor(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   return (
-    <IconButton {...props}>
-      <ColorLensIcon />
-    </IconButton>
+    <div>
+      <IconButton
+        {...props}
+        onClick={(event) => setAnchorEl(event.currentTarget)}
+      >
+        <ColorLensIcon />
+      </IconButton>
+      <Popover
+        open={anchorEl}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        onClose={() => setAnchorEl(null)}
+      >
+        <CirclePicker
+          width="250px"
+          onChangeComplete={props.addColor}
+          colors={colors}
+        />
+      </Popover>
+    </div>
   );
 }
