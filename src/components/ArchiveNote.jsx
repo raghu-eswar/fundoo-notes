cc70d14f05c198cb812fe3151ebd846b97a604bf
raghueswar55 <raghueswar55@gmail.com>
2020-09-13 17:03:02 +0530

    [ Raghu ] REFACTORED: modified to change icon on toggle.

--- src/components/ArchiveNote.jsx
+++ src/components/ArchiveNote.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import ArchiveIcon from '@material-ui/icons/Archive';
+import ArchiveIcon from "@material-ui/icons/Archive";
+import UnarchiveIcon from "@material-ui/icons/Unarchive";
 
 export default function ArchiveNote(props) {
   return (
-    <IconButton {...props}>
-      <ArchiveIcon />
+    <IconButton {...props} onClick={props.toggleArchive}>
+      {props.isArchived ? <UnarchiveIcon /> : <ArchiveIcon />}
     </IconButton>
   );
 }
