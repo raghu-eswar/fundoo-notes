77a5524ded5c8bdd6f5500a65174483753703536
raghueswar55 <raghueswar55@gmail.com>
2020-09-13 08:54:59 +0530

    [ Raghu ] REFACTORED: modified to change color on toggle.

--- src/components/PinNote.jsx
+++ src/components/PinNote.jsx
@@ -4,8 +4,8 @@ import Icon from '@material-ui/core/Icon';
 
 export default function PinNote(props) {
   return (
-    <IconButton {...props}>
-      <Icon className="fa fa-thumb-tack"/>
+    <IconButton {...props} onClick={props.togglePin}>
+      <Icon className="fa fa-thumb-tack" style={{color: props.isPined?'#3f51b5':'#0000008a'}}/>
     </IconButton>
   );
 }
