760d12044a8b6574ba08f1238df5a209c9b45850
raghueswar55 <raghueswar55@gmail.com>
2020-09-14 20:26:23 +0530

    [ Raghu ] ADD: implemented chip to display reminder.

---
+++ src/components/ReminderChip.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Chip from "@material-ui/core/Chip";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+
+export default function ReminderChip(props) {
+  let lable = "";
+  let date = new Date(props.reminder);
+  let temp = date.getDate() - new Date().getDate();
+  switch (temp) {
+    case 0:
+      lable =
+        "To Day " +
+        date.toLocaleString([], { hour: "2-digit", minute: "2-digit" });
+      break;
+    case 1:
+      lable =
+        "Tomorrow " +
+        date.toLocaleString([], { hour: "2-digit", minute: "2-digit" });
+      break;
+    default:
+      lable =
+        date.toLocaleString("en-us", { month: "short" }) +
+        " " +
+        date.toLocaleString("en-us", { day: "numeric" }) +
+        " " +
+        date.toLocaleString([], { hour: "2-digit", minute: "2-digit" });
+      break;
+  }
+
+  return (
+    <Chip
+      icon={<AccessAlarmIcon />}
+      label={lable}
+      onDelete={props.deleteReminder}
+    />
+  );
+}
