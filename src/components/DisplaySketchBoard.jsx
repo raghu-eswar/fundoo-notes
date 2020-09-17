c68f414fb147bc2c17e4478ff787f8d160ad1d7d
raghueswar55 <raghueswar55@gmail.com>
2020-09-18 00:14:55 +0530

    [ Raghu ] ADD: implemented sketch board to display saved sketches.

---
+++ src/components/DisplaySketchBoard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { SketchField } from "react-sketch";
+import * as Styled from "../styles/displaySketchBoard.styled";
+
+export default function DisplaySketchBoard(props) {
+  const [drawings, setDrawings] = React.useState(props.drawing);
+  const sketch = React.useRef();
+  React.useEffect(() => {
+    setDrawings(props.drawing);
+    setTimeout(() => {
+      sketch.current.zoom(props.zoom);
+    }, 150);
+  }, [props.drawing, props.zoom]);
+
+  return (
+    <Styled.SketchBoardContainer maxWidth="sm" onClick={props.openSketchBoard}>
+      <SketchField
+        ref={sketch}
+        backgroundColor={props.drawing.backgroundColor}
+        width={props.width}
+        height={props.height}
+        value={drawings}
+      />
+    </Styled.SketchBoardContainer>
+  );
+}
