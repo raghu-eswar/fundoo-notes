ce2698ee06a9b8822f3404c8e6b5d877675decce
raghueswar55 <raghueswar55@gmail.com>
2020-09-19 12:19:22 +0530

    [ Raghu ] REFACTOR: modified icon color.

--- src/components/PinNote.jsx
+++ src/components/PinNote.jsx
@@ -5,7 +5,7 @@ import Icon from '@material-ui/core/Icon';
 export default function PinNote(props) {
   return (
     <IconButton {...props} onClick={props.togglePin}>
-      <Icon className="fa fa-thumb-tack" style={{color: props.isPined?'#3f51b5':'#0000008a'}}/>
+      <Icon className="fa fa-thumb-tack" style={{color: props.isPined?'#000000':'#0000008a'}}/>
     </IconButton>
   );
 }
