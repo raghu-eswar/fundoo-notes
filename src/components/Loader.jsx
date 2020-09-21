36204356162e04c5d73064391a4e08267c49b45e
raghueswar55 <raghueswar55@gmail.com>
2020-09-22 01:07:35 +0530

    [ Raghu ] REFACTOR: modified  loader animations.

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,40 +3,56 @@ import Model from "@material-ui/core/Modal";
 import styled, { keyframes } from "styled-components";
 
 const Loading = keyframes`
-    0%{transform: rotate(90deg) translateX(40px);  visibility: visible;}
-    5%{background-color: #ffd700;}
-    10%{background-color: #8b0000;}
-    15%{background-color: #8a2be2;}
-    20%{background-color: #00ffff;}
-    25%{background-color: #ffa500;}
-    30%{background-color: #008000;}
-    35%{background-color: #ff00bf;}
-    40%{background-color: #800080;}
-    50%{transform: rotate(2160deg) translateX(80px);}
-    55%{background-color: #ffd700;}
-    60%{background-color: #8b0000;}
-    65%{background-color: #8a2be2;}
-    70%{background-color: #00ffff;}
-    75%{background-color: #ffa500;}
-    85%{background-color: #7fff00;}
-    100%{background-color: #ffd700;}
+    0% { transform: scale(0.0); background-color: #ffbe0b; }
+    25% {background-color: #fb5607}
+    50% {background-color: #ff006e; transform: scale(1);}
+    75%{background-color: #8338ec}
+    100% { transform: scale(0.0)  rotateY(180deg); background-color: #3a86ff;}
 `;
 
-const Dot = styled.span`
-  width: 10px;
-  height: 10px;
-  background-color: #0000ff;
-  border-radius: 50%;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  visibility: hidden;
-  animation: ${Loading} 10s linear infinite;
-  animation-delay: ${(props) => props.delay};
+const Container = styled.div`
+  outline: none;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 5px;
+  column-gap: 5px;
+  width: 100px;
+  height: 100px;
+
+  > div {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    background: white;
+    transform: scale(0);
+    transform-origin: center center;
+    animation: ${Loading} 1.5s infinite linear;
+    border-radius: 5px;
+
+    &:nth-child(1),
+    &:nth-child(5),
+    &:nth-child(9) {
+      animation-delay: 0.4s;
+    }
+    &:nth-child(4),
+    &:nth-child(8) {
+      animation-delay: 0.2s;
+    }
+    &:nth-child(2),
+    &:nth-child(6) {
+      animation-delay: 0.6s;
+    }
+    &:nth-child(3) {
+      animation-delay: 0.8s;
+    }
+  }
 `;
 
 const StyledModel = styled(Model)`
-  & > div {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & > div:first-child {
     background-color: rgba(255, 255, 255, 0.5) !important;
   }
 `;
@@ -44,18 +60,17 @@ const StyledModel = styled(Model)`
 export default function Loader(props) {
   return (
     <StyledModel open={props.open}>
-      <>
-        <Dot delay={"0s"}></Dot>
-        <Dot delay={"0.3s"}></Dot>
-        <Dot delay={"0.6s"}></Dot>
-        <Dot delay={"0.9s"}></Dot>
-        <Dot delay={"1.2s"}></Dot>
-        <Dot delay={"1.5s"}></Dot>
-        <Dot delay={"1.8s"}></Dot>
-        <Dot delay={"2.1s"}></Dot>
-        <Dot delay={"2.4s"}></Dot>
-        <Dot delay={"2.7s"}></Dot>
-      </>
+      <Container>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Container>
     </StyledModel>
   );
 }
