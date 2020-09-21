3b4519a470f239fad04d350c51d74e818efa1ec6
raghueswar55 <raghueswar55@gmail.com>
2020-09-21 10:20:31 +0530

    [ Raghu ] REFACTOR: modified code indentation.

--- src/services/notesServices.js
+++ src/services/notesServices.js
@@ -1,60 +1,103 @@
-import axios from 'axios';
-import { notesApiConstants } from '../apiConstants/notesApiConstants';
+import axios from "axios";
+import { notesApiConstants } from "../apiConstants/notesApiConstants";
 
 export function addNotes(note, token) {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.addNotes, note, {
-        headers: {Authorization: token,}
-    })
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.addNotes,
+    note,
+    {
+      headers: { Authorization: token },
+    }
+  );
 }
 
 export function getNotesList(token) {
-    return axios.get(process.env.REACT_APP_API_BASE_URL + notesApiConstants.getNotesList, {
-        headers: {Authorization: token,}
-    })
+  return axios.get(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.getNotesList,
+    {
+      headers: { Authorization: token },
+    }
+  );
 }
 
 export function updateNotes(note, token) {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.updateNotes, note, {
-        headers: {Authorization: token,}
-    })
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.updateNotes,
+    note,
+    {
+      headers: { Authorization: token },
+    }
+  );
 }
 export function changeNoteColor(data, token) {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.changesColorNotes, data, {
-        headers: {Authorization: token,}
-    })
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.changesColorNotes,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
 }
-export const pinUnpinNotes = (data, token)=> {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.pinUnpinNotes, data, {
-        headers: {Authorization: token,}
-    })
-}
-export const archiveNotes = (data, token)=> {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.archiveNotes, data, {
-        headers: {Authorization: token,}
-    })
-}
-export const trashNotes = (data, token)=> {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.trashNotes, data, {
-        headers: {Authorization: token,}
-    })
-}
-export const addUpdateReminderNotes = (data, token)=> {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.addUpdateReminderNotes, data, {
-        headers: {Authorization: token,}
-    })
-}
-export const removeReminderNotes = (data, token)=> {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.removeReminderNotes, data, {
-        headers: {Authorization: token,}
-    })
-}
-export const noteLabels = (data, token)=> {
-    return axios.post(process.env.REACT_APP_API_BASE_URL + notesApiConstants.noteLabels, data, {
-        headers: {Authorization: token,}
-    })
-}
-export const getNoteLabelList = (token)=> {
-    return axios.get(process.env.REACT_APP_API_BASE_URL + notesApiConstants.getNoteLabelList, {
-        headers: {Authorization: token,}
-    })
-}
+export const pinUnpinNotes = (data, token) => {
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.pinUnpinNotes,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
+export const archiveNotes = (data, token) => {
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.archiveNotes,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
+export const trashNotes = (data, token) => {
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.trashNotes,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
+export const addUpdateReminderNotes = (data, token) => {
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL +
+      notesApiConstants.addUpdateReminderNotes,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
+export const removeReminderNotes = (data, token) => {
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.removeReminderNotes,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
+export const noteLabels = (data, token) => {
+  return axios.post(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.noteLabels,
+    data,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
+export const getNoteLabelList = (token) => {
+  return axios.get(
+    process.env.REACT_APP_API_BASE_URL + notesApiConstants.getNoteLabelList,
+    {
+      headers: { Authorization: token },
+    }
+  );
+};
