93a2e6f8a27f836d3316a6395b19c02d1770393a
raghueswar55 <raghueswar55@gmail.com>
2020-09-21 10:06:45 +0530

    [ Raghu ] ADD: implemented component to update labels.

---
+++ src/components/UpdateLabel.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import LabelIcon from "@material-ui/icons/Label";
+import EditIcon from "@material-ui/icons/Edit";
+import IconButton from "@material-ui/core/IconButton";
+import DoneIcon from "@material-ui/icons/Done";
+import * as Styled from "../styles/editLabels.styled";
+
+export default function UpdateLabels(props) {
+  const [focus, setFocus] = React.useState(false);
+  const [label, setLabel] = React.useState(props.label);
+  const input = React.useRef();
+
+  const handleFocus = () => {
+    if (!focus) {
+      input.current.focus();
+      setFocus(true);
+    }
+  };
+
+  return (
+    <Styled.LableContainer>
+      <IconButton
+        onMouseOver={() => setFocus(true)}
+        onMouseOut={() => setFocus(false)}
+      >
+        {focus ? <DeleteIcon /> : <LabelIcon />}
+      </IconButton>
+      <Styled.StyledTextField
+        onFocus={() => setFocus(true)}
+        onBlur={() => setFocus(false)}
+        ref={input}
+        value={label.label}
+        onChange={(event) => setLabel({ ...label, label: event.target.value })}
+      />
+      {!focus && (
+        <IconButton onClick={handleFocus}>
+          <EditIcon />
+        </IconButton>
+      )}
+      {focus && (
+        <IconButton>
+          <DoneIcon />
+        </IconButton>
+      )}
+    </Styled.LableContainer>
+  );
+}
