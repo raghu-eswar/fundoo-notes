a388723c3a33a76a9913aff0d004c324a33ac2b2
raghueswar55 <raghueswar55@gmail.com>
2020-09-23 17:13:35 +0530

    [ Raghu ] REFACTOR: added add label option.

--- src/components/MoreNoteOptions.jsx
+++ src/components/MoreNoteOptions.jsx
@@ -3,6 +3,7 @@ import IconButton from "@material-ui/core/IconButton";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import Menu from "@material-ui/core/Menu";
 import DeleteOrRestoreNoteOption from "./DeleteOrRestoreNoteOption";
+import AddLabelsOption from "./AddLabelsOption";
 
 export default function MoreNoteOptions(props) {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -20,12 +21,32 @@ export default function MoreNoteOptions(props) {
       <IconButton {...props} onClick={openMenu}>
         <MoreVertIcon />
       </IconButton>
-      <Menu anchorEl={anchorEl} keepMounted open={anchorEl} onClose={closeMenu}>
+      <Menu
+        anchorEl={anchorEl}
+        keepMounted
+        open={anchorEl}
+        onClose={closeMenu}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
         {props.deleteNote && (
           <DeleteOrRestoreNoteOption deleteNote={props.deleteNote} />
         )}
         {props.restoreNote && (
           <DeleteOrRestoreNoteOption restoreNote={props.restoreNote} />
+        )}
+        {props.addLabels && props.labels && (
+          <AddLabelsOption
+            labels={props.labels}
+            addLabels={props.addLabels}
+            removeLabels={props.removeLabels}
+          />
         )}
       </Menu>
     </>
