9938a4071cee05701c8774e6712639825c39e2ce
raghueswar55 <raghueswar55@gmail.com>
2020-09-23 17:01:41 +0530

    [ Raghu ] REFACTOR: increased width.

--- src/components/DeleteOrRestoreNoteOption.jsx
+++ src/components/DeleteOrRestoreNoteOption.jsx
@@ -3,7 +3,10 @@ import MenuItem from "@material-ui/core/MenuItem";
 
 export default function DeleteOrRestoreNoteOption(props) {
   return (
-    <MenuItem onClick={props.deleteNote ? props.deleteNote : props.restoreNote}>
+    <MenuItem
+      onClick={props.deleteNote ? props.deleteNote : props.restoreNote}
+      style={{ width: "10rem" }}
+    >
       {props.deleteNote ? "Delete Note" : "Restore"}
     </MenuItem>
   );
