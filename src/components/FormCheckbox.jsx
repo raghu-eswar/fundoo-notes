d555da027d70951c1b2cec87411ce5a9d0484778
raghueswar55 <raghueswar55@gmail.com>
2020-09-23 16:39:16 +0530

    [ Raghu ] ADD: implemented label with check box option.

---
+++ src/components/FormCheckbox.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+
+export default function FormCheckbox(props) {
+  const [checked, setChecked] = React.useState(props.checked);
+
+  const onChangeHandler = (event) => {
+    setChecked(event.target.checked);
+    if (event.target.checked) props.onCheck(props.label);
+    else props.onUnCheck(props.label);
+  };
+  return (
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={checked}
+          color="primary"
+          onChange={onChangeHandler}
+        />
+      }
+      label={props.label.label}
+    />
+  );
+}
