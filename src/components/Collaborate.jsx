935f97419da9abb088e57aff152755c2ba8ea7d5
raghueswar55 <raghueswar55@gmail.com>
2020-10-02 19:48:52 +0530

    [ Raghu ] REFACTOR: implemented feature to display user list.

--- src/components/Collaborate.jsx
+++ src/components/Collaborate.jsx
@@ -1,11 +1,67 @@
 import React from "react";
 import IconButton from "@material-ui/core/IconButton";
-import GroupAddOutlinedIcon from '@material-ui/icons/GroupAddOutlined';
+import GroupAddOutlinedIcon from "@material-ui/icons/GroupAddOutlined";
+import Popover from "@material-ui/core/Popover";
+import Typography from "@material-ui/core/Typography";
+import FormCheckbox from "./FormCheckbox";
+import * as Styled from "../styles/collaborate.styled";
+import { searchUserList } from "../services/notesServices";
 
 export default function Collaborate(props) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [collaborators, setCollaborators] = React.useState([]);
+
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const closeMenu = () => {
+    setCollaborators([]);
+    setAnchorEl(null);
+  };
+
+  const searchUsers = (event) => {
+    var value = event.target.value;
+    if (value) {
+      let data = { searchWord: value };
+      searchUserList(data, props.token).then((response) => {
+        if (response.data.data.success)
+          setCollaborators(response.data.data.details);
+      });
+    } else setCollaborators([]);
+  };
   return (
-    <IconButton {...props}>
-      <GroupAddOutlinedIcon />
-    </IconButton>
+    <>
+      <IconButton {...props} onClick={openMenu}>
+        <GroupAddOutlinedIcon />
+      </IconButton>
+      <Popover
+        anchorEl={anchorEl}
+        open={anchorEl}
+        onClose={closeMenu}
+        anchorOrigin={{
+          vertical: "center",
+          horizontal: "right",
+        }}
+      >
+        <Styled.MainContainer>
+          <Typography variant="h6" align="left">
+            add collaborators :
+          </Typography>
+          <Styled.SearchContainer>
+            <Styled.Input
+              placeholder="search"
+              autoFocus
+              onChange={searchUsers}
+            />
+          </Styled.SearchContainer>
+          <Styled.LabelsContainer>
+            {collaborators.map((collaborator) => (
+              <FormCheckbox label={collaborator.email} />
+            ))}
+          </Styled.LabelsContainer>
+        </Styled.MainContainer>
+      </Popover>
+    </>
   );
 }
---
+++ src/styles/collaborate.styled.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const MainContainer = styled.div`
+  width: auto;
+  padding: 1rem;
+`;
+export const SearchContainer = styled.div`
+  height: 3rem;
+  margin-bottom: 5px;
+`;
+export const LabelsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-height: 15rem;
+  overflow: auto;
+`;
+export const Input = styled.input`
+  height: 85%;
+  width: 95%;
+  font-size: larger;
+  border: none;
+  outline: none;
+  border-bottom: 1px solid gray;
+`;
